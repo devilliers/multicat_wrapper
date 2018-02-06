ede5be11586fd310bb53176edbd28a458650d9f9
Simon de Villiers <simon.devilliers@bt.com>
2018-02-06 13:50:19 +0000

    [master] added in argparser and staggered threads

--- run_multicat.py
+++ run_multicat.py
@@ -1,44 +1,85 @@
-import sys
+import argparse
 import os
-from concurrent.futures import ProcessPoolExecutor
-from typing import Tuple
+from concurrent.futures import ProcessPoolExecutor, as_completed
+from typing import List
+import time
+from itertools import chain
+
 
 # initialise with default values
-TS_FILE = 'BT_Barker-Promo-HDp25_5MBps.ts'
-PCR_PID = '33'
-STREAM_COUNT = 1
-IP_ADDR_TARGET = '0.0.0.0'
-PORT_TARGET = '5000'
-
-# set values to argv values
-try:
-    TS_FILE, PCR_PID, STREAM_COUNT = sys.argv[1:]
-except ValueError:
-    print(f'\nUsage: \n\tpython3.6 run_multicat.py TS_FILE PCR_PID STREAM_COUNT\n')
-    accept_defaults = input(
-        f'Use default values of TS_FILE={TS_FILE}, PCR_PID={PCR_PID} and STREAM_COUNT={STREAM_COUNT}? [y/n] ')
-    if accept_defaults == 'n':
-        print('\n')
-        sys.exit()
+stream_count = 10
+ip_addr_target = '0.0.0.0'
+port_target = 5000
+stagger_milliseconds = 500
 
 
-def multicat_thread(multicat_values: Tuple):
+def first_ts_file():
+    """ Finds the first .ts
+        file in the current directory
+    """
+    return next((f for f in os.listdir(os.getcwd()) if f.endswith('.ts'))) or 'no .ts files found'
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--file', help='name of .ts file (defaults to first found in directory)',
+                    default=first_ts_file())
+parser.add_argument('--pid', help='PCR PID of ts file',  type=int, default=33)
+parser.add_argument(
+    '--threads', help='thread count (instances of multicat)', default=10, type=int)
+parser.add_argument(
+    '--ip', help='target ip address (of encoder)', default='0.0.0.0')
+parser.add_argument(
+    '--port', help='starting target port number', type=int, default=5000)
+parser.add_argument(
+    '--ms', help='milliseconds to stagger launching each instance of multicat by', type=int, default=500)
+
+parser = parser.parse_args()
+
+port_target = parser.port
+
+# print out config
+print(f"""Using values:
+    ts file = {parser.file}
+    pcr pid = {parser.pid}
+    thread count = {parser.threads}
+    target ip address = {parser.ip}
+    initial port number = {port_target}
+    milliseconds stagger = {parser.ms}""")
+
+# s --> ms
+parser.ms /= 1000
+
+
+def multicat_thread(multicat_values: List):
     """ Run multicat in a process thread with above values
     :param details: 3-tuple of values to pass to multicat
     """
-    TS_FILE, PCR_PID, STREAM_COUNT = multicat_values
+    global port_target, values_message
+    ts_file, pcr_pid, stream_count, ip_addr_target = multicat_values
     try:
-        # os.system(f'ingests -p {PCR_PID} {TS_FILE}')
-        # os.system(f'multicat -X -U {TS_FILE} {IP_ADDR_TARGET}:{PORT_TARGET}')
-        print('running multicat')
+        # os.system(f'ingests -p {pcr_pid} {ts_file}')
+        # os.system(
+        #     f'multicat -X -U {ts_file} {ip_addr_target}:{str(port_target)}')
+        print(f"""Using values:
+    ts file = {parser.file}
+    pcr pid = {parser.pid}
+    thread count = {parser.threads}
+    target ip address = {parser.ip}
+    initial port number = {port_target}
+    milliseconds stagger = {parser.ms}""")
     except Exception as e:
         print(str(e))
+    port_target += 1
+    return port_target - 1
 
 
-# make the Pool of workers
-pool = ProcessPoolExecutor(max_workers=100)
-
-iterator = [(TS_FILE, PCR_PID, STREAM_COUNT) for _ in range(50)]
-
-# map function to each thread in the pool
-results = pool.map(multicat_thread, iterator)
+# info on pool.submit() found here https://stackoverflow.com/questions/42074501/python-concurrent-futures-processpoolexecutor-performance-of-submit-vs-map#42081890
+with ProcessPoolExecutor(max_workers=100) as pool:
+    futures = []
+    while parser.threads > 0:
+        time.sleep(parser.ms)
+        futures.append(pool.submit(multicat_thread, [
+                       parser.file, parser.pid, parser.threads, parser.ip]))
+        parser.threads -= 1
+    print([f.result() for f in as_completed(futures)])
+    # print(chain.from_iterable(f.result() for f in as_completed(futures)))

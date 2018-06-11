42b1962b08cf76773f41d53b5e6ca93c4b1bec53
Simon de Villiers <simondv66@gmail.com>
2018-06-11 14:55:37 +0100

    refactored to use main func

--- parallel_multicats.py
+++ parallel_multicats.py
@@ -22,19 +22,20 @@ def signal_handler(signal, frame):
     interrupted = True
 
 
-signal.signal(signal.SIGINT, signal_handler)
-
-
 def multiple_file_types(*patterns):
     return it.chain.from_iterable(glob.iglob(pattern) for pattern in patterns)
 
 
-def first_ts_file():
-    """ Finds the first .ts
-        file in the current directory
+def first_file(ftype):
+    """ Finds the first file in the current directory
+    of the specified type
     """
+    type_relationship = {
+        'ts': ['*.ts', '*.mpg'],
+        'csv': ['*.csv'],
+    }
     try:
-        return next((filename for filename in multiple_file_types('*.ts', '*.mpg')))
+        return next((filename for filename in multiple_file_types(type_relationship[ftype])))
     except StopIteration:
         raise FileNotFoundError(
             'no transport stream files found in current directory')
@@ -62,98 +63,6 @@ class WFileList(list):
 
     def __repr__(self):
         return ''
-
-
-parser = argparse.ArgumentParser()
-group = parser.add_mutually_exclusive_group(required=True)
-# these are the flags for this script
-group.add_argument('--file', '-f', help='Name of transport stream file to use (defaults to first found in directory).',
-                   default=first_ts_file())
-group.add_argument('--manifest', '-m',
-                   help='Name of manifest file containing list of ts files and corresponding weights.')
-parser.add_argument('--pid', help='PCR PID of ts file.',
-                    type=int, default=33)
-parser.add_argument(
-    '--threads', '-t', help='Thread count (instances of multicat).', default=1, type=int)
-parser.add_argument(
-    '--ip', '-i', help='Connect IPv4 address (of encoder).', default='0.0.0.0')
-parser.add_argument(
-    '--port', '-p', help='Starting connect port number.', type=int, default=5001)
-parser.add_argument(
-    '--bip', help='Bind IPv4 address (of encoder).', default='10.10.111.2')
-parser.add_argument(
-    '--bport', help='Starting bind port number.', type=int)
-parser.add_argument(
-    '--ms', help='Milliseconds to stagger launching each instance of multicat by.', type=int, default=500)
-parser.add_argument('--incr_ip', action='store_true',
-                    help='Set last number in connect IPv4 address to increment with each thread spawned.')
-parser.add_argument('--incr_port', action='store_true',
-                    help='Set connect port to increment with each thread spawned.')
-parser.add_argument('--loop', '-l', action='store_true')
-parser.add_argument('--RTP', type=str)
-parser.add_argument('--ttl', type=int)
-
-# these are the flags to pass to straight to Multicat, that would be used when
-# running Multicat without this wrapper script. Those that allow a variable to be
-# passed in are processed below.
-parser.add_argument(
-    '--flags', nargs='+',
-    help='''Flags to pass to multicat (just use the letters themselves, without "-");
-    run ```$ multicat --help``` for info on flags.''', default=[], choices=['X', 'T', 'f', 'p', 'C', 'P', 's', 'n',
-                                                                            'k', 'd', 'a', 'r', 'O', 'S', 'u', 'U',
-                                                                            'm', 'R', 'w', ])
-
-parser = parser.parse_args()
-
-###### Start Manifest file processing #########
-
-if parser.manifest:
-    mf = parser.manifest
-    if not mf.endswith('.csv'):
-        raise FileNotFoundError('manifest file must be in CSV format')
-    weighted_files = WFileList()
-    with open(mf, 'rb') as csvfile:
-        for row in reader(csvfile):
-            WFileList.append(tuple(row))
-
-###### End of Manifest file processing #########
-
-###### Start Multicat flag processing #########
-
-# if these are flags (like --ttl, for example) are used, the corresponding flags
-# need to be passed to Multicat
-flags_to_add_to_multicat = {
-    parser.ttl: 't',
-    parser.RTP: 'u'
-}
-for flag in flags_to_add_to_multicat:
-    if flag:
-        parser.flags.append(flags_to_add_to_multicat[flag])
-
-# pass any flag variables through to multicat flags that use them
-
-mc_flag_additions = {
-    '-T': '-T {xml_output}'.format(xml_output=parser.file.replace(".ts", ".xml")),
-    '-t': '-t {ttl}'.format(ttl=parser.ttl),
-    '-u': '-u {rtp}'.format(rtp=parser.RTP),
-}
-for i, flag in enumerate(parser.flags):
-    parser.flags[i] = '-' + flag
-    for k, v in mc_flag_additions.items():
-        if parser.flags[i] == k:
-            parser.flags[i] = v
-
-###### End of Multicat flag processing #########
-
-# used in outputting info
-CONNECT_IP = parser.ip
-CONNECT_PORT = parser.port
-BIND_IP = parser.bip
-BIND_PORT = parser.bport
-TOTAL_THREADS = parser.threads
-
-# ms --> s
-parser.ms /= 1000
 
 
 def increment_ip(ip: str) -> str:
@@ -184,7 +93,7 @@ def ingest_ts(pcr_pid: int, ts_file: str):
 
 def build_execution_string(cip: str, cport: int, bip: str=None, bport: int=None) -> str:
     flags = " ".join(parser.flags)
-    execution_string = 'multicat {flags} {fl} '.format(flags=flags, fl=parser.file) + \
+    execution_string = 'multicat {flags} {fl} '.format(flags=flags, fl=parser.file) +
         '{cip}:{cport}'.format(cip=cip, cport=str(cport))
     additions = {
         parser.bip: '@{bip}'.format(bip=bip),
@@ -238,19 +147,118 @@ def multicat_thread(multicat_values: list):
         print(str(e))
 
 
-# Generate threads with multicat
-with ProcessPoolExecutor(max_workers=TOTAL_THREADS) as pool:
-    futures = []
-    thread_no = TOTAL_THREADS
-    while parser.threads > 0:
-        thread_no = TOTAL_THREADS - parser.threads + 1
-        time.sleep(parser.ms)
-        futures.append(pool.submit(multicat_thread, [
-                       thread_no, parser.file, parser.pid, parser.ms, parser.flags,
-                       CONNECT_IP, CONNECT_PORT, BIND_IP, BIND_PORT]))
-        if parser.incr_port:
-            CONNECT_PORT += 1
-        if parser.incr_ip:
-            CONNECT_IP = increment_ip(CONNECT_IP)
-        parser.threads -= 1
+def main():
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group(required=True)
+    # these are the flags for this script
+    group.add_argument(
+        '--file', '-f', help='Name of transport stream file to use (defaults to first found in directory).')
+    group.add_argument('--manifest', '-m',
+                       help='Name of manifest file containing list of ts files and corresponding weights.')
+    parser.add_argument('--pid', help='PCR PID of ts file.',
+                        type=int, default=33)
+    parser.add_argument(
+        '--threads', '-t', help='Thread count (instances of multicat).', default=1, type=int)
+    parser.add_argument(
+        '--ip', '-i', help='Connect IPv4 address (of encoder).', default='0.0.0.0')
+    parser.add_argument(
+        '--port', '-p', help='Starting connect port number.', type=int, default=5001)
+    parser.add_argument(
+        '--bip', help='Bind IPv4 address (of encoder).', default='10.10.111.2')
+    parser.add_argument(
+        '--bport', help='Starting bind port number.', type=int)
+    parser.add_argument(
+        '--ms', help='Milliseconds to stagger launching each instance of multicat by.', type=int, default=500)
+    parser.add_argument('--incr_ip', action='store_true',
+                        help='Set last number in connect IPv4 address to increment with each thread spawned.')
+    parser.add_argument('--incr_port', action='store_true',
+                        help='Set connect port to increment with each thread spawned.')
+    parser.add_argument('--loop', '-l', action='store_true')
+    parser.add_argument('--RTP', type=str)
+    parser.add_argument('--ttl', type=int)
+
+    # these are the flags to pass to straight to Multicat, that would be used when
+    # running Multicat without this wrapper script. Those that allow a variable to be
+    # passed in are processed below.
+    parser.add_argument(
+        '--flags', nargs='+',
+        help='''Flags to pass to multicat (just use the letters themselves, without "-");
+        run ```$ multicat --help``` for info on flags.''', default=[], choices=['X', 'T', 'f', 'p', 'C', 'P', 's', 'n',
+                                                                                'k', 'd', 'a', 'r', 'O', 'S', 'u', 'U',
+                                                                                'm', 'R', 'w', ])
+
+    parser = parser.parse_args()
+
+    signal.signal(signal.SIGINT, signal_handler)
+
+    ###### Start Manifest file processing #########
+
+    if parser.manifest:
+        mf = parser.manifest
+        if not mf.endswith('.csv'):
+            raise FileNotFoundError('manifest file must be in CSV format')
+        weighted_files = WFileList()
+        with open(mf, 'rb') as csvfile:
+            for row in reader(csvfile):
+                weighted_files.append(tuple(row))
+
+    ###### End of Manifest file processing #########
+
+    ###### Start Multicat flag processing #########
+
+    # if these are flags (like --ttl, for example) are used, the corresponding flags
+    # need to be passed to Multicat
+    flags_to_add_to_multicat = {
+        parser.ttl: 't',
+        parser.RTP: 'u'
+    }
+    for flag in flags_to_add_to_multicat:
+        if flag:
+            parser.flags.append(flags_to_add_to_multicat[flag])
+
+    # pass any flag variables through to multicat flags that use them
+    mc_flag_additions = {
+        '-T': '-T {xml_output}'.format(xml_output=parser.file.replace(".ts", ".xml")),
+        '-t': '-t {ttl}'.format(ttl=parser.ttl),
+        '-u': '-u {rtp}'.format(rtp=parser.RTP),
+    }
+    for i, flag in enumerate(parser.flags):
+        parser.flags[i] = '-' + flag
+        for k, v in mc_flag_additions.items():
+            if parser.flags[i] == k:
+                parser.flags[i] = v
+
+    ###### End of Multicat flag processing #########
+
+    # used in outputting info
+    CONNECT_IP = parser.ip
+    CONNECT_PORT = parser.port
+    BIND_IP = parser.bip
+    BIND_PORT = parser.bport
+    TOTAL_THREADS = parser.threads
+
+    # ms --> s
+    parser.ms /= 1000
+
+    # Generate threads with multicat
+    with ProcessPoolExecutor(max_workers=TOTAL_THREADS) as pool:
+        futures = []
         thread_no = TOTAL_THREADS
+        while parser.threads > 0:
+            thread_no = TOTAL_THREADS - parser.threads + 1
+            time.sleep(parser.ms)
+            # elect a file based on weightings if using manifest (if not, use specified file)
+            file_choice = weighted_files.elect() if parser.manifest else parser.file
+            futures.append(pool.submit(multicat_thread, [
+                thread_no, file_choice, parser.pid, parser.ms, parser.flags,
+                CONNECT_IP, CONNECT_PORT, BIND_IP, BIND_PORT]))
+            if parser.incr_port:
+                CONNECT_PORT += 1
+            if parser.incr_ip:
+                CONNECT_IP = increment_ip(CONNECT_IP)
+            parser.threads -= 1
+            thread_no = TOTAL_THREADS
+
+
+if __name__ == '__main__':
+    main()
